61195be368999d049723cc3b877493508fbfdc7d
cloudnote2027 <cloud@nomor.shop>
2026-06-10 22:46:19 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
 from config import BOT_TOKEN
 from handlers import register_handlers
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
 
 async def main():
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher(storage=MemoryStorage())
     register_handlers(dp)
-    print("✅ VPN Bot Started!")
-    print("📡 Connected to Appwrite Cloud")
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
