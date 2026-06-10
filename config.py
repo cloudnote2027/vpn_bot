f349b1206671d740dac11162699d1fee2aba7d70
cloudnote2027 <cloud@nomor.shop>
2026-06-10 22:42:00 +0630

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ COLLECTION_OUTLINE_POOL = "outline_pool"
 COLLECTION_V2RAY_POOL = "v2ray_pool"
 COLLECTION_CREDIT_REQUESTS = "credit_requests"
 
-REGISTER_CREDITS = int(os.getenv("REGISTER_CREDITS", 5))
+REGISTER_CREDITS = int(os.getenv("REGISTER_CREDITS", 1))      # 1 credit for new user
 REFER_CREDITS = int(os.getenv("REFER_CREDITS", 10))
 OUTLINE_KEY_COST = int(os.getenv("OUTLINE_KEY_COST", 5))
 V2RAY_KEY_COST = int(os.getenv("V2RAY_KEY_COST", 8))
@@ -25,3 +25,6 @@ V2RAY_KEY_COST = int(os.getenv("V2RAY_KEY_COST", 8))
 SERVER_NAME = os.getenv("SERVER_NAME", "Lord Kazuma VPN")
 SERVER_LOCATION = os.getenv("SERVER_LOCATION", "Singapore")
 CHANNEL_URL = os.getenv("CHANNEL_URL", "https://t.me/yourchannel")
+
+# Static refer base URL (without ?start=...)
+REFER_BASE_URL = "https://t.me/bug303"
